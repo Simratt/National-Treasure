ce3131b7b398c336fe38bfc995fd8dcc54400193
SimratBains <simrat.bains@mail.utoronto.ca>
2022-02-06 07:56:01 -0500

    The backend functionality is finally working, whenever more users are added to the app, it is automatically updated. Just need to sort the users by score and all that is left is makign it look pretty

--- national_treasure/src/App.js
+++ national_treasure/src/App.js
@@ -1,17 +1,52 @@
 import './App.css';
 import Box from './components/box';
-import React, {useState} from 'react';
+import React,{useState, useEffect } from 'react';
+
+// Import the SDKs
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getFirestore, collection, getDocs} from '@firebase/firestore'
+import { addConsoleHandler } from 'selenium-webdriver/lib/logging';
+const firebaseConfig = {
+  apiKey: "AIzaSyDqym_VegUzxBGmT-8vNMDq0LFqhhIxBNA",
+  authDomain: "national-treasure-7040e.firebaseapp.com",
+  projectId: "national-treasure-7040e",
+  storageBucket: "national-treasure-7040e.appspot.com",
+  messagingSenderId: "868705212959",
+  appId: "1:868705212959:web:ac29d889809f696c30d32a",
+  measurementId: "G-7HGRXTRLW3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 
 function App() {
-  const [board, setBoard] = useState([])
-  function addItem() {
-    setBoard([...board, <Box text="User" />])
+  const temp = [];
+  const [users, setUsers] = useState([]);
+  const board = [];
+  //load the users from the firebase store
+  const db = getFirestore(app)
+  const usersData = collection(db, "users")
+  
+  useEffect(() => {
+    const getUsers = async() => {
+      const data = await getDocs(usersData)
+      setUsers(data.docs.map(ele => ({...ele.data(), id: ele.id})))
+    } 
+    getUsers()
+  }, [])
+  for(const e in users){
+    temp.push([users[e].name, users[e].score])
   }
+  for(const element in temp){
+    board.push(<Box arr = {temp[element]}/>)
+  }
+  
   return (
     <div className="App">
       <h1 id='title'>LEADERBOARD</h1>
-      <button onClick={addItem}>click me</button>
-      
       <ul>
         {board.map(b => (
           <li>{b}</li>))}
--- national_treasure/src/components/box.js
+++ national_treasure/src/components/box.js
@@ -2,9 +2,16 @@ import './box.css'
 import React from 'react'
 
 function Box(props) {
+    const data = [];
+    for(const e in props){
+        data.push(props[e])
+    }
     return (
-        <p id='boardItem'>{props.text}</p>
+        <div className='boardElement'>
+            <div>{data[0][0]}</div>
+            <div id="spacer"></div>
+            <div>score: {data[0][1]}</div>
+        </div>
     )
 }
-
 export default Box;
